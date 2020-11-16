8c2bcdacd3b78e30dc35affd45d1302d733ea028
黃冠霖 <dolphins1123@gmail.com>
2020-11-16 10:19:01 +0800

    upd

--- src/app/pages/tables/smart-table/smart-table.component.ts
+++ src/app/pages/tables/smart-table/smart-table.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import { LocalDataSource } from 'ng2-smart-table';
+import { Component } from "@angular/core";
+import { LocalDataSource } from "ng2-smart-table";
 
-import { SmartTableData } from '../../../@core/data/smart-table';
+import { SmartTableData } from "../../../@core/data/smart-table";
 
 @Component({
-  selector: 'ngx-smart-table',
-  templateUrl: './smart-table.component.html',
-  styleUrls: ['./smart-table.component.scss'],
+  selector: "ngx-smart-table",
+  templateUrl: "./smart-table.component.html",
+  styleUrls: ["./smart-table.component.scss"],
 })
 export class SmartTableComponent {
-
   settings = {
     add: {
       addButtonContent: '<i class="nb-plus"></i>',
@@ -26,85 +25,87 @@ export class SmartTableComponent {
       confirmDelete: true,
     },
     columns: {
-
- 
       id: {
-        title: 'ID',
-        type: 'number',
+        title: "ID",
+        type: "number",
+        filter: true,
       },
       firstName: {
-        title: 'First Name',
-        type: 'string',
+        title: "First Name",
+        type: "string",
+        filter: false,
       },
       lastName: {
-        title: 'Last Name',
-        type: 'string',
+        title: "Last Name",
+        type: "string",
+        filter: false,
       },
       username: {
-        title: 'Username',
-        type: 'string',
+        title: "Username",
+        type: "string",
+        filter: false,
+
       },
       email: {
-        title: 'E-mail',
-        type: 'string',
+        title: "E-mail",
+        type: "string",
+        filter: false,
       },
       age: {
-        title: 'Age',
-        type: 'number',
+        title: "Age",
+        type: "number",
+        filter: true,
       },
 
-      Actions: //or something
-  {
-    title:'Detail',
-    type:'html',
-    valuePrepareFunction:(cell,row)=>{
-      return `<a title="詳細" href="Your api key or something/${row.Id}"> <i class="fa fa-eye"></i></a>`
-    },
-    filter:false       
-  },
+      //or something
+      Actions: {
+        title: "Detail",
+        type: "html",
+        valuePrepareFunction: (cell, row) => {
+          return `<a title="詳細" href="Your api key or something/${row.Id}"> <i class="fa fa-eye"></i></a>`;
+        },
+        filter: false,
+      },
     },
   };
 
   source: LocalDataSource = new LocalDataSource();
 
   constructor(private service: SmartTableData) {
-   // const data = this.service.getData();
-   // this.source.load(data);
-    this.queryData()
+  //  const data = this.service.getData();
+  //   this.source.load(data);
+
+    this.queryData();
   }
 
   onDeleteConfirm(event): void {
-    if (window.confirm('Are you sure you want to delete?')) {
+    if (window.confirm("Are you sure you want to delete?")) {
       event.confirm.resolve();
     } else {
       event.confirm.reject();
     }
   }
 
-//query data
-  queryData(): void {
-  
+  // query data
+   queryData(): void{
 
-    let foo:any[any]
-    fetch(
-      'https://my.api.mockaroo.com/smarttable.json?key=416e4a40'
- //     , {}
+  fetch(
+      "https://my.api.mockaroo.com/smarttable.json?key=416e4a40"
     )
-       .then((res) => {
-    res.json().then((jsondata) => {
+      .then((res) => {
+        res.json().then((jsondata) => {
+         // foo =  jsondata;
+          console.log('jsondata=',jsondata)
+         this.source.load(jsondata);
+        //  return jsondata;
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-       this.source.load(jsondata);
-  
 
-    });
-})
-.catch((err) => {
-  console.log(err)
-
-});
- 
 
   }
 
 }
-
